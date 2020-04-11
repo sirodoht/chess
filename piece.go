3b2b47368b08b5f3b57b642c3e5a655df5cd8098
Theodore Keloglou <zf@sirodoht.com>
2020-04-11 17:04:32 +0100

    add get piece stuff

--- piece.go
+++ piece.go
@@ -17,3 +17,45 @@ const (
 	// KING is the king chess piece of a chess board
 	KING
 )
+
+// GetPiece returns the Piece type given a piece notation
+// e.g. "P" -> PAWN
+func GetPiece(pieceNotation rune) Piece {
+	pieces := map[rune]Piece{
+		'P': PAWN,
+		'R': ROOK,
+		'K': KNIGHT,
+		'B': BISHOP,
+		'Q': QUEEN,
+		'G': KING,
+	}
+	return pieces[pieceNotation]
+}
+
+// GetPieceName returns the name of the piece
+// e.g. PAWN -> "Pawn"
+func GetPieceName(piece Piece) string {
+	pieceNames := map[Piece]string{
+		PAWN:   "Pawn",
+		ROOK:   "Rook",
+		KNIGHT: "Knight",
+		BISHOP: "Bishop",
+		QUEEN:  "Queen",
+		KING:   "King",
+	}
+	return pieceNames[piece]
+}
+
+// GetPieceAbbr returns the abbreviation of the piece
+// e.g. PAWN -> "P"
+func GetPieceAbbr(piece Piece) string {
+	pieceNames := map[Piece]string{
+		PAWN:   "P",
+		ROOK:   "R",
+		KNIGHT: "K",
+		BISHOP: "B",
+		QUEEN:  "Q",
+		KING:   "G",
+	}
+	return pieceNames[piece]
+}
