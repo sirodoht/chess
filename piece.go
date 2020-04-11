feeacb6297e1a04a9cea45a41d67026ea40dc056
Theodore Keloglou <zf@sirodoht.com>
2020-04-11 20:39:25 +0100

    refactor get possible moves to different functions

--- piece.go
+++ piece.go
@@ -58,4 +58,137 @@ func GetPieceAbbr(piece Piece) string {
 		KING:   "G",
 	}
 	return pieceNames[piece]
+// GetPossibleRookMoves returns all possible moves for a Rook,
+// given current location on the board
+func GetPossibleRookMoves(origin Location) []Location {
+	possibleMoves := []Location{}
+
+	// same column, for all rows
+	newRow := 0
+	for newRow < 8 {
+		if newRow == origin.row {
+			// omit current location
+			newRow++
+			continue
+		}
+		possibleMoves = append(possibleMoves, Location{
+			row: newRow,
+			col: origin.col,
+		})
+		newRow++
+	}
+
+	// same row, for all columns
+	newCol := 0
+	for newCol < 8 {
+		if newCol == origin.col {
+			// omit current location
+			newCol++
+			continue
+		}
+		possibleMoves = append(possibleMoves, Location{
+			row: origin.row,
+			col: newCol,
+		})
+		newCol++
+	}
+
+	return possibleMoves
+}
+
+// GetPossibleKnightMoves returns all possible moves for a Knight,
+// given current location on the board
+func GetPossibleKnightMoves(origin Location) []Location {
+	// searching for Knight moves in a fashion
+	// of two hops forward, then one left, or one right
+	possibleMoves := []Location{}
+
+	// handle top hand
+	newRow := origin.row - 2
+	if newRow >= 0 {
+		newCol := origin.col - 1
+		// check for left side
+		if newCol >= 0 {
+			possibleMoves = append(possibleMoves, Location{
+				row: newRow,
+				col: newCol,
+			})
+		}
+		newCol = origin.col + 1
+		// check for right side
+		if newCol <= 7 {
+			possibleMoves = append(possibleMoves, Location{
+				row: newRow,
+				col: newCol,
+			})
+		}
+	}
+
+	// handle right hand
+	newCol := origin.col + 2
+	if newCol <= 7 {
+		newRow := origin.row - 1
+		// check for top side
+		if newRow >= 0 {
+			possibleMoves = append(possibleMoves, Location{
+				row: newRow,
+				col: newCol,
+			})
+		}
+
+		newRow = origin.row + 1
+		// check for bottom side
+		if newRow <= 7 {
+			possibleMoves = append(possibleMoves, Location{
+				row: newRow,
+				col: newCol,
+			})
+		}
+	}
+
+	// handle bottom hand
+	newRow = origin.row + 2
+	if newRow <= 7 {
+		newCol := origin.col - 1
+		// check for left side
+		if newCol >= 0 {
+			possibleMoves = append(possibleMoves, Location{
+				row: newRow,
+				col: newCol,
+			})
+		}
+
+		newCol = origin.col + 1
+		// check for right side
+		if newCol <= 7 {
+			possibleMoves = append(possibleMoves, Location{
+				row: newRow,
+				col: newCol,
+			})
+		}
+	}
+
+	// handle left hand
+	newCol = origin.col - 2
+	if newCol >= 0 {
+		newRow := origin.row - 1
+		// check for top side
+		if newRow <= 7 {
+			possibleMoves = append(possibleMoves, Location{
+				row: newRow,
+				col: newCol,
+			})
+		}
+
+		newRow = origin.row + 1
+		// check for botom side
+		if newRow >= 0 {
+			possibleMoves = append(possibleMoves, Location{
+				row: newRow,
+				col: newCol,
+			})
+		}
+	}
+
+	return possibleMoves
 }
