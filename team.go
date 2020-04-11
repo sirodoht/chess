d0a6e2adf3ae900e2178e96cfa031bba7ac6173d
Theodore Keloglou <zf@sirodoht.com>
2020-04-11 17:04:22 +0100

    add get team name and symbol

--- team.go
+++ team.go
@@ -9,3 +9,23 @@ const (
 	// BLACK is the black team / player on the chess board
 	BLACK
 )
+
+// GetTeamName returns the name of the team
+// e.g. WHITE -> "white ○"
+func GetTeamName(team Team) string {
+	TeamNames := map[Team]string{
+		WHITE: "white ○",
+		BLACK: "black ●",
+	}
+	return TeamNames[team]
+}
+
+// GetTeamSymbol returns the symbol / circle of the team
+// e.g. WHITE -> "○"
+func GetTeamSymbol(team Team) string {
+	TeamNames := map[Team]string{
+		WHITE: "○",
+		BLACK: "●",
+	}
+	return TeamNames[team]
+}
