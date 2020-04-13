2fc8e231fcbd9f4474f94c86cc6977d0376f9d92
Theodore Keloglou <zf@sirodoht.com>
2020-04-13 21:24:04 +0100

    add initial board test

---
+++ board_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSimplePawnMove(t *testing.T) {
+	board := Board{}
+	board.Init()
+
+	// create move
+	turn := WHITE
+	command := "e7 e5"
+	move, _ := NewMove(board, turn, command)
+
+	// execute move
+	board.Execute(move)
+
+	// verify piece moved
+	locationBefore := move.GetLocation(BEFORE)
+	locationAfter := move.GetLocation(AFTER)
+	if board[locationBefore.row][locationBefore.col] != "   " {
+		t.Error("white pawn did not move out of e7")
+	}
+	if board[locationAfter.row][locationAfter.col] != "○ P" {
+		t.Error("white pawn did not move into e5")
+	}
+}
