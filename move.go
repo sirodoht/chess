8efa1ea5bee392eb8077c06fff17813baa7d1f31
Theodore Keloglou <zf@sirodoht.com>
2020-04-13 17:14:55 +0100

    fix wrong column mapping

--- move.go
+++ move.go
@@ -70,14 +70,14 @@ func (m Move) GetLocation(part Part) Location {
 
 	// col
 	columnLetters := map[rune]int{
-		'a': 1,
-		'b': 2,
-		'c': 3,
-		'd': 4,
-		'e': 5,
-		'f': 6,
-		'g': 7,
-		'h': 8,
+		'a': 0,
+		'b': 1,
+		'c': 2,
+		'd': 3,
+		'e': 4,
+		'f': 5,
+		'g': 6,
+		'h': 7,
 	}
 	col := columnLetters[m.beforeLetter]
 	if part == AFTER {
